385f19932b5d4a9d21ed2d3ad2f57443daf55e38
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2023-11-07 12:24:45 +0000

    blog post link changes

--- wp-content/themes/Lubov/components/latest_blog_post.php
+++ wp-content/themes/Lubov/components/latest_blog_post.php
@@ -6,6 +6,8 @@ $title = get_the_title();
 $image = get_the_post_thumbnail();
 $date = get_the_date(' d F Y');
 
+$externalLink = get_field('external_post_link');
+
 // $truncate_title = truncate($title, 65);
 
 $categories = null;
@@ -22,7 +24,22 @@ $concat = get_query_var( 'concat' );
 ?>
 
 <article class="latest-blog-post" data-aos="fade-up">
-  <a href="<?php echo $link; ?>" class="latest-blog-post-inner" aria-label="Blog post">
+  <a href="<?php if($externalLink):?><?php echo $externalLink; ?><?php else:?><?php echo $link;?> <?php endif; ?>" class="latest-blog-post-inner" aria-label="Blog post">
+  <div class="blog-top">
+      <div class="image-container">
+      <?php the_post_thumbnail('single-post-thumbnail'); ?>
+    </div>
+    <div class="blog-bottom">
+        <div class="blog-title mt-3">
+            <h3 class="title"><?php echo $title;?></h3>
+        </div>
+        <?php if(has_excerpt()):?><div class="excerpt"><p><?php echo get_the_excerpt(); ?></p></div><?php endif; ?>
+      <div class="post-info">
+        <p class="date"><?php echo get_the_date('j F');?></p>
+      </div>
+    </div>
+</a>
+  <!-- <a href="<?php echo $link;?>" class="latest-blog-post-inner" aria-label="Blog post">
     <div class="blog-top">
       <div class="image-container">
       <?php the_post_thumbnail('single-post-thumbnail'); ?>
@@ -36,5 +53,5 @@ $concat = get_query_var( 'concat' );
         <p class="date"><?php echo get_the_date('j F');?></p>
       </div>
     </div>
-  </a>
+  </a> -->
 </article>
