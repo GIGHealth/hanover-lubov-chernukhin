cc4352e4630da6b84478ee0f5b2bb4aba2040e5d
Rob Hern <robertjhern@gmail.com>
2023-12-01 10:11:10 +0000

    link to body copy

--- wp-content/themes/Lubov/components/content-rows/double_column.php
+++ wp-content/themes/Lubov/components/content-rows/double_column.php
@@ -23,13 +23,13 @@ if($link){
   <div
     class="pt-3 pb-3 double-column-inner d-flex  <?php if($image_placement):?>flex-lg-row-reverse <?php else: ?>flex-lg-row <?php endif; ?> flex-column content-bounds">
     <div class="d-flex double-column-inner-copy flex-column justify-content-center align-items-center w-100"
-      <?php if(!$image_placement):?><?php endif; ?> <?php if(!$image_placement):?> <?php endif; ?>  data-aos="fade-up">
+      <?php if(!$image_placement):?><?php endif; ?> <?php if(!$image_placement):?> <?php endif; ?> data-aos="fade-up">
       <div class="content<?php if($title): ?> pt-3<?php endif;?>">
         <?php if($title): ?>
         <h3 class="mb-4"><?php echo $title; ?></h3>
         <?php endif; ?>
         <?php if($copy):?>
-          <?php echo $copy; ?>
+        <?php echo $copy; ?>
         <?php endif; ?>
         <?php if ($slideCopy): ?>
         <div class="slide-down-content mb-3">
@@ -37,25 +37,25 @@ if($link){
             <span class="slide-link"><?php echo $slideLink; ?></span>
             <?php arrowRight()?>
             <div class="slide-copy mt-3"> <?php echo $slideCopy; ?>
-          </a> 
+          </a>
           <a class="slide-link-close-container" aria-label="Close read more section" href="javascript:void(0)">
             <span class="slide-link-close">Close</span>
             <?php arrowRight()?>
           </a>
-          </div>
         </div>
-        <?php if($copy2): ?>
-        <?php echo $copy2; ?>
-        <?php endif; ?>
-        <?php endif;?>
-        <?php if($link):?>
-        <div class="link-container">
-          <?php echo buttonLink($link_url, $link_target, $link_text); ?>
-        </div>
-        <?php endif; ?>
       </div>
+      <?php if($copy2): ?>
+      <?php echo $copy2; ?>
+      <?php endif; ?>
+      <?php endif;?>
+      <?php if($link):?>
+      <div class="link-container">
+        <?php echo buttonLink($link_url, $link_target, $link_text); ?>
+      </div>
+      <?php endif; ?>
     </div>
-    <?php
+  </div>
+  <?php
     
     if( have_rows('image') ):
         if(is_array($imgRepeater)) {
@@ -65,23 +65,25 @@ if($link){
         $count = 1;
         }
     ?>
-    <div class="carousel-wrap position-relative"  data-aos="fade-up" data-aos-delay="100">
-      <div
-        class="image-container image-carousel-inner double-column w-100 <?php if($count > 1): ?>owl-carousel owl-theme<?php endif; ?> <?php if($count === 1): ?><?php if($parallax):?>parallax<?php else: ?>parallax-reverse<?php endif; ?><?php endif; ?>"
-        <?php if($count):?>data-count="<?php echo $count; ?><?php endif;?>">
-        <?php while( have_rows('image') ): the_row(); 
+  <div class="carousel-wrap position-relative" data-aos="fade-up" data-aos-delay="100">
+    <div
+      class="image-container image-carousel-inner double-column w-100 <?php if($count > 1): ?>owl-carousel owl-theme<?php endif; ?> <?php if($count === 1): ?><?php if($parallax):?>parallax<?php else: ?>parallax-reverse<?php endif; ?><?php endif; ?>"
+      <?php if($count):?>data-count="<?php echo $count; ?><?php endif;?>">
+      <?php while( have_rows('image') ): the_row(); 
           $img = get_sub_field('image_item');
 
           ?>
-          <div class="image-wrapper position-relative">
-            <img class="image image-position-carousel image-position-<?php echo esc_html ( get_sub_field('image_position') ); ?>" src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>"
-            width="<?php echo $img['width']; ?>" height="<?php echo $img['height']; ?>" />
-            </div>
-
-          <?php endwhile; ?>
-      
+      <div class="image-wrapper position-relative">
+        <img
+          class="image image-position-carousel image-position-<?php echo esc_html ( get_sub_field('image_position') ); ?>"
+          src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" width="<?php echo $img['width']; ?>"
+          height="<?php echo $img['height']; ?>" />
       </div>
+
+      <?php endwhile; ?>
+
     </div>
-    <?php endif; ?>
   </div>
+  <?php endif; ?>
+</div>
 </div>
